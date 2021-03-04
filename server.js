f417b24e2b4d4d2d2db9355042a4925f8d9490c1
ali1129js <ali1129js@gmail.com>
2021-03-04 11:59:10 +0100

    formating

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 import express from "express";
 import cors from "cors";
+
+// app init
 const app = express();
+
 // add cors
 app.use(cors());
+
 //add middleware
 app.get("/", (req, res) => {
   res.send(data);
 });
 
 //fake data
-
 const data = [
   {
     text: "Task five",
-    id: 1,
+    id: 5,
     isCompleted: false,
   },
   {
     text: "Task nine",
-    id: 2,
+    id: 9,
     isCompleted: false,
   },
 ];
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Express is running on port : ${PORT}`));
